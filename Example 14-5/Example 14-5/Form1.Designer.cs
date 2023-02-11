8502562177000af985dcbc764e6916604dad3143
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 18:47:46 +0330

    Add files via upload

---
+++ Example 14-5/Example 14-5/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Example_14_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFont = new System.Windows.Forms.Button();
+            this.txtFont = new System.Windows.Forms.TextBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(170, 156);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(106, 26);
+            this.btnFont.TabIndex = 1;
+            this.btnFont.Text = "Change Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // txtFont
+            // 
+            this.txtFont.Location = new System.Drawing.Point(25, 25);
+            this.txtFont.Multiline = true;
+            this.txtFont.Name = "txtFont";
+            this.txtFont.Size = new System.Drawing.Size(251, 125);
+            this.txtFont.TabIndex = 0;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.FontMustExist = true;
+            this.fontDialog1.ShowColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ctrl+Shift+F";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 210);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFont);
+            this.Controls.Add(this.btnFont);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Example 14-5 (Font Dialog)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.TextBox txtFont;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Example 14-5/Example 14-5/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_14_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //----------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            fontDialog1.ShowColor = true;
+            fontDialog1.FontMustExist = true;
+        }
+        //----------------------------------------------------------------
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.Shift && e.KeyCode==Keys.F)
+            {
+                SetFont();
+            }
+        }
+        //----------------------------------------------------------------
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            SetFont();
+        }
+        //----------------------------------------------------------------
+        private void SetFont()//new method
+        {
+            fontDialog1.Font = txtFont.Font;
+            fontDialog1.Color = txtFont.ForeColor;
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFont.Font = fontDialog1.Font;
+                txtFont.ForeColor = fontDialog1.Color;
+            }
+            txtFont.Focus();
+        }
+    }
+}
